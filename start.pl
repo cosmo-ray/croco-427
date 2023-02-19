60cc56cd1235c6c7a689d401a9f6b837695bd02a
Matthias Gatto <uso.cosmo.ray@gmail.com>
2023-02-19 09:53:43 +0100

    open animation done ?
    Signed-off-by: Matthias Gatto <uso.cosmo.ray@gmail.com>

--- start.pl
+++ start.pl
@@ -9,6 +9,49 @@
 my $cur_txt_img;
 my $cur_cnt;
 
+my $original_time;
+my $time_acc;
+
+sub enter_action
+{
+    if ($time_acc eq 0) {
+	$time_acc = 1;
+	return
+    }
+    my $SLIDE_L = 1000000;
+    $next_acc = $time_acc + Yirl::ywidGetTurnTimer();
+    $sld_pos = $time_acc / $SLIDE_L;
+    $sld_true_pos = $next_acc / $SLIDE_L;
+    $nb_slide=scalar @door_open;
+
+    if ($sld_true_pos != $sld_pos and $sld_true_pos < $nb_slide) {
+	Yirl::yeReCreateString(@door_open[$sld_true_pos], $cur_txt_img, "text");
+	if (int($sld_true_pos) == 5) {
+	    Yirl::ywReplaceEntry2(
+		$cur_cnt,
+		Yirl::yaeString("rgba: 255 155 155 255",
+				Yirl::ywTextScreenNew("You: so, what now ?"),
+				"background"),
+		1);
+
+	} elsif (int($sld_true_pos) == 6) {
+	    Yirl::ywReplaceEntry2(
+		$cur_cnt,
+		Yirl::yaeString("rgba: 155 255 155 255",
+				Yirl::ywTextScreenNew("Akira: look, a map"),
+				"background"),
+		1);
+	} elsif (int($sld_true_pos) == $nb_slide - 1) {
+	    print("BEGIN YOOOOOOO ? \n");
+	    Yirl::yePrint($map_dialogue);
+	    print("END YOOOOOOO ? \n");
+	    Yirl::ywReplaceEntry2($cur_cnt, $map_dialogue, 1);
+	    Yirl::yeRemoveChildByStr($cur_cnt, "action");
+	}
+    }
+    $time_acc = $next_acc;
+}
+
 sub console_action
 {
     my $input_wid = $_[0];
@@ -17,17 +60,26 @@ sub console_action
 
     #Yirl::yePrint($input_wid);
     #print($txt . "\n");
-    Yirl::yePrint(Yirl::ywCntGetEntry($container, 0));
     Yirl::yesCall(Yirl::ygGet("text_input_reset"), $input_wid);
     print "-----\n";
     if (uc($txt) eq "HELP") {
 	Yirl::yeReCreateString($console_help, $cur_txt_img, "text");
     } elsif (uc($txt) eq "DRAW") {
 	Yirl::yeReCreateString($console_draw, $cur_txt_img, "text");
+    } elsif (uc($txt) eq "OPEN") {
+	Yirl::yeReCreateString($door_open[0], $cur_txt_img, "text");
+	Yirl::ywReplaceEntry2(
+	    $cur_cnt,
+	    Yirl::yaeString("rgba: 155 155 155 255",
+			    Yirl::ywTextScreenNew("doors are open..."),
+			    "background"),
+	    1);
+	Yirl::yeCreateFunction("enter_action", $cur_cnt, "action");
+	Yirl::ywSetTurnLengthOverwrite(-1);
+	$time_acc = 0;
     } else {
 	Yirl::yeReCreateString($console_unknow, $cur_txt_img, "text");
     }
-    print "--oo--\n";
     return 1;
 }
 
@@ -37,8 +89,6 @@ sub do_console
 
     Yirl::yeReCreateString($console, $cur_txt_img, "text");
     print("-------------------===========-----------------------\n");
-    Yirl::yePrint($wid);
-    print("= new ts =\n");
     Yirl::ywReplaceEntry2(
 	$cur_cnt,
 	Yirl::yaeString(
@@ -59,9 +109,6 @@ sub widget_init
 {
     $wid = $_[0];
     print "widget_init !!!!!!\n";
-    do "/usr/share/cows/kitty.cow";
-    $thoughts='o';
-    print $the_cow;
     do "./story.pl";
     print $door;
     print $test;
@@ -90,7 +137,6 @@ sub widget_init
 sub mod_init
 {
     $mod = $_[0];
-    Yirl::yePrint($mod);
     print("in mod !\n");
     $callback = Yirl::yeCreateFunction("widget_init");
     print("UwU !\n");
@@ -111,7 +157,10 @@ sub mod_init
 	);
     Yirl::yeCreateFunction("console_action", $input, "on-enter");
     Yirl::ygInitWidgetModule($mod, "croco-427", $callback);
-    Yirl::yePrint($mod);
+    #need a destroy callback that rest that.
+    $original_time = Yirl::ywGetTurnLengthOverwrite();
+
+    Yirl::ywSetTurnLengthOverwrite(0);
     return $mod;
 }
 
--- story.pl
+++ story.pl
@@ -1,5 +1,7 @@
+print "sprites: \n";
 do "./sprites.pl";
 
+print "done\n";
 $door_dialogue = Yirl::yeCreateArray();
 Yirl::yeCreateString("rgba: 255 255 255 255", $door_dialogue, "background");
 Yirl::yeCreateString("dialogue", $door_dialogue, "<type>");
@@ -21,4 +23,23 @@ $new_txt = Yirl::yeCreateArray($answer, "action");
 Yirl::yeCreateString("Dialogue.change-text", $new_txt);
 Yirl::yeCreateString("The door look doorestque", $new_txt);
 
-Yirl::yePrint($answers);
+$map_dialogue = Yirl::yeCreateArray();
+Yirl::yeCreateString("rgba: 255 255 255 255", $map_dialogue, "background");
+Yirl::yeCreateString("dialogue", $map_dialogue, "<type>");
+Yirl::yeCreateInt(20, $map_dialogue, "txt-size");
+Yirl::yeCreateInt(1, $map_dialogue, "is_looner_dialogue");
+$map_dialogue_2 = Yirl::yeCreateArray($map_dialogue, "dialogue");
+Yirl::yeCreateString("what to do", $map_dialogue_2, "text");
+$answers = Yirl::yeCreateArray($map_dialogue_2, "answers");
+
+$answer = Yirl::yeCreateArray($answers);
+Yirl::yeCreateString("Go to the console", $answer, "text");
+$new_txt = Yirl::yeCreateArray($answer, "action");
+Yirl::yeCreateString("croco-427.do_console", $new_txt);
+Yirl::yeCreateInt(10, $new_txt);
+
+$answer = Yirl::yeCreateArray($answers);
+Yirl::yeCreateString("Look at the door", $answer, "text");
+$new_txt = Yirl::yeCreateArray($answer, "action");
+Yirl::yeCreateString("Dialogue.change-text", $new_txt);
+Yirl::yeCreateString("The door look doorestque", $new_txt);
